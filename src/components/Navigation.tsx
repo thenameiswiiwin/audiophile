1676f500035b104b676924805ee5fdcdbb1deb1a
Huy Nguyen <huy@Huys-MacBook-Pro.local>
2022-09-22 01:01:36 -0500

    responsive for Hero and Navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -11,15 +11,15 @@ const LINKS = [
 
 function Navigation() {
   return (
-    <nav className="border-b border-white/10 px-6 py-8 md:px-0 xl:py-9">
-      <div className="flex items-center justify-between md:justify-start md:gap-11 xl:justify-between">
-        <button className="xl:hidden" type="button">
+    <nav className="border-b border-white/10 mx-6 2xl:mx-[165px] py-8 2xl:py-9">
+      <div className="flex items-center justify-between md:justify-start md:gap-11 2xl:justify-between">
+        <button className="2xl:hidden" type="button">
           <img src={Hamburger} alt="hamburger icon" />
         </button>
         <a href="/">
           <img src={Logo} alt="audiophile logo" />
         </a>
-        <div className="hidden xl:block">
+        <div className="hidden 2xl:block">
           <ul className="flex gap-9">
             {LINKS.map((link) => (
               <li
@@ -31,7 +31,7 @@ function Navigation() {
             ))}
           </ul>
         </div>
-        <button className="md:ml-auto xl:ml-0" type="button">
+        <button className="md:ml-auto 2xl:ml-0" type="button">
           <img src={Cart} alt="cart icon" />
         </button>
       </div>
